06a3d64c3184a48896d70485e2f4eb86480eb329
As'ad Saleh Umar <asad@rubyh.co>
2019-11-30 06:28:58 +0700

    Setup basic graphql

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,19 @@ import React from "react";
 import Router from "./Router";
 import { Provider } from "react-redux";
 import store from "./redux";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+const client = new ApolloClient({
+  uri: "http://mandalorian.rubyh.co/graphql"
+});
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <Router />
+      <ApolloProvider client={client}>
+        <Router />
+      </ApolloProvider>
     </Provider>
   );
 };
