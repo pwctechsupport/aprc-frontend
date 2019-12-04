da371309f4b4e4bb8e725bd7a2dd4897dcf60ca6
djarkobobanov <djarkobobanov@gmail.com>
2019-12-04 18:56:11 +0700

    Create Business Process Create and delete

--- src/containers/businessProcess/BusinessProcesses.tsx
+++ src/containers/businessProcess/BusinessProcesses.tsx
@@ -10,11 +10,14 @@ import { Link } from "react-router-dom";
 import Button from "../../shared/components/Button";
 import { FaTrash } from "react-icons/fa";
 import { toast } from "react-toastify";
+import CreateBusinessProcess from "./CreateBusinessProcess";
 
 const BusinessProcesses = () => {
-  const boby = useBusinessProcessesQuery({ variables: { filter: {} } });
-  const boba = oc(boby).data.businessProcesses.collection([]);
-  const [destroy, bodel] = useDestroyBusinessProcessMutation({
+  const businessQuery = useBusinessProcessesQuery({
+    variables: { filter: {} }
+  });
+  const bPCollection = oc(businessQuery).data.businessProcesses.collection([]);
+  const [destroy] = useDestroyBusinessProcessMutation({
     onCompleted: () => toast.success("Delete Success"),
     onError: () => toast.error("Delete Failed"),
     refetchQueries: [
@@ -27,32 +30,43 @@ const BusinessProcesses = () => {
   };
 
   return (
-    <Table>
-      <thead>
-        <tr>
-          <th>Business Process</th>
-          <th>Business Process ID</th>
-          <th>Parent</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {boba.map(item => (
+    <div>
+      <div className="d-flex justify-content-between align-items-center">
+        <h1>Business Process </h1>
+      </div>
+      <div>
+        <CreateBusinessProcess />
+      </div>
+      <Table>
+        <thead>
           <tr>
-            <td>
-              <Link to={`/business-process/${item.id}`}>{item.name}</Link>{" "}
-            </td>
-            <td> {item.id}</td>
-            <td> {item.ancestry}</td>
-            <td>
-              <Button onClick={() => handleDelete(item.id)} color="transparent">
-                <FaTrash />
-              </Button>
-            </td>
+            <th>Business Process</th>
+            <th>Business Process ID</th>
+            <th>Parent</th>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {bPCollection.map(item => (
+            <tr>
+              <td>
+                <Link to={`/business-process/${item.id}`}>{item.name}</Link>
+              </td>
+              <td> {item.id}</td>
+              <td> {item.ancestry}</td>
+              <td>
+                <Button
+                  onClick={() => handleDelete(item.id)}
+                  color="transparent"
+                >
+                  <FaTrash />
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   );
 };
 
--- src/containers/businessProcess/CreateBusinessProcess.tsx
+++ src/containers/businessProcess/CreateBusinessProcess.tsx
@@ -1,7 +1,48 @@
-import React from "react"
+import React from "react";
+import useForm from "react-hook-form";
+import { Form, Input } from "reactstrap";
+import Button from "../../shared/components/Button";
+import {
+  useCreateBusinessProcessMutation,
+  BusinessProcessesDocument
+} from "../../generated/graphql";
+import { toast } from "react-toastify";
 
 const CreateBusinessProcess = () => {
-  return <div>hallo test</div>
-}
+  const { register, handleSubmit, reset } = useForm<CreateBPFormValues>();
+  const [createBP] = useCreateBusinessProcessMutation({
+    onCompleted: () => {
+      toast.success("Create Success");
+      reset();
+    },
+    onError: () => toast.error("Create Failed"),
+    refetchQueries: [
+      { query: BusinessProcessesDocument, variables: { filter: {} } }
+    ]
+  });
+  const submit = (values: CreateBPFormValues) => {
+    createBP({ variables: { input: { name: values.name } } });
+  };
+  return (
+    <Form
+      onSubmit={handleSubmit(submit)}
+      className="d-flex align-items-center mb-4"
+    >
+      <Input
+        name="name"
+        placeholder="Business Process Name"
+        innerRef={register}
+        required
+      />
+      <Button type="submit" className="pwc ml-3">
+        Add
+      </Button>
+    </Form>
+  );
+};
 
-export default CreateBusinessProcess
+export default CreateBusinessProcess;
+
+interface CreateBPFormValues {
+  name: string;
+}
