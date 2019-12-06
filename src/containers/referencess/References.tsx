a4b0d7a9f45e5bc26ed19c80ef8fc5e11b7fb11a
Hanggi Anggono <anggonohanggi@gmail.com>
2019-12-06 15:08:47 +0700

    Search references

--- src/containers/referencess/References.tsx
+++ src/containers/referencess/References.tsx
@@ -1,55 +1,67 @@
-import React from 'react';
-import { oc } from 'ts-optchain';
-import { useReferencesQuery, useDestroyReferenceMutation } from '../../generated/graphql';
-import Table from '../../shared/components/Table';
-import CreateReference from './CreateReference';
-import { Button } from 'reactstrap';
+import React, { useState } from 'react';
 import { FaTrash } from 'react-icons/fa';
 import { toast } from 'react-toastify';
+import { Button } from 'reactstrap';
+import { oc } from 'ts-optchain';
+import { useDebounce } from 'use-debounce/lib';
+import { useDestroyReferenceMutation, useReferencesQuery } from '../../generated/graphql';
+import Table from '../../shared/components/Table';
+import ControlSideBox from '../control/components/ControlSideBox';
+import CreateReference from './CreateReference';
 
 const References = () => {
-  const { data, loading } = useReferencesQuery()
+  const [searchValue, setSearchValue] = useState("");
+  const [searchQuery] = useDebounce(searchValue, 700);
+
+  const { data, loading } = useReferencesQuery({variables: {filter: { name_cont: searchQuery }}})
   const [destroyReference] = useDestroyReferenceMutation({
     refetchQueries: ['references'],
     onCompleted: () => toast.success('Delete Success'),
     onError: () => toast.error('Delete Failed')
   })
 
+  const handleChange = (event: any) => {
+    setSearchValue(event.target.value);
+  }
+
   return (
-    <div>
-      <div className="d-flex justify-content-between align-items-center">
-        <h1>References</h1>
-      </div>
+    <div className="d-flex">
+      <ControlSideBox searchValue={searchValue} handleChange={handleChange} />
+      <div className="ml-3 flex-grow-1">
+        <div className="d-flex justify-content-between align-items-center">
+          <h1>References</h1>
+        </div>
 
-      <div>
-        <CreateReference/>
-      </div>
+        <div>
+          <CreateReference />
+        </div>
 
-      <Table loading={loading}>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th/>
-          </tr>
-        </thead>
-        <tbody>
-          {oc(data).references.collection([]).map(reference => {
-            return (
-              <tr key={reference.id}>
-                <td>{reference.name}</td>
-                <td>
-                  <Button
-                    onClick={() => destroyReference({variables: {id: reference.id}})}
-                    color="transparent"
-                  >
-                    <FaTrash/>
-                  </Button>
-                </td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </Table>
+        <Table loading={loading}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {oc(data).references.collection([]).map(reference => {
+              return (
+                <tr key={reference.id}>
+                  <td>{reference.name}</td>
+                  <td>
+                    <Button
+                      onClick={() => destroyReference({ variables: { id: reference.id } })}
+                      color="transparent"
+                    >
+                      <FaTrash />
+                    </Button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </Table>
+      </div>
     </div>
   )
 }
