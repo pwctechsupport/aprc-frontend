69b09a052d61bedf09ef540f680e5f1aa9254272
djarkobobanov <djarkobobanov@gmail.com>
2019-12-06 14:04:22 +0700

    Navigation Box Business Process

--- src/containers/businessProcess/BusinessProcesses.tsx
+++ src/containers/businessProcess/BusinessProcesses.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   useBusinessProcessesQuery,
   useDestroyBusinessProcessMutation,
@@ -11,18 +11,24 @@ import Button from "../../shared/components/Button";
 import { FaTrash } from "react-icons/fa";
 import { toast } from "react-toastify";
 import CreateBusinessProcess from "./CreateBusinessProcess";
+import BusinessProcessSideBox from "./components/BusinessProcessSideBox";
+import { useDebounce } from "use-debounce/lib";
 
 const BusinessProcesses = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [searchQuery] = useDebounce(searchValue, 700);
+
+  const handleChange = (event: any) => {
+    setSearchValue(event.target.value);
+  };
   const businessQuery = useBusinessProcessesQuery({
-    variables: { filter: {} }
+    variables: { filter: { name_cont: searchQuery } }
   });
   const bPCollection = oc(businessQuery).data.businessProcesses.collection([]);
   const [destroy] = useDestroyBusinessProcessMutation({
     onCompleted: () => toast.success("Delete Success"),
     onError: () => toast.error("Delete Failed"),
-    refetchQueries: [
-      { query: BusinessProcessesDocument, variables: { filter: {} } }
-    ]
+    refetchQueries: ["businessProcesses"]
   });
 
   const handleDelete = (id: string) => {
@@ -30,42 +36,48 @@ const BusinessProcesses = () => {
   };
 
   return (
-    <div>
-      <div className="d-flex justify-content-between align-items-center">
-        <h1>Business Process </h1>
-      </div>
-      <div>
-        <CreateBusinessProcess />
-      </div>
-      <Table>
-        <thead>
-          <tr>
-            <th>Business Process</th>
-            <th>Business Process ID</th>
-            <th>Parent</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {bPCollection.map(item => (
-            <tr key={item.id}>
-              <td>
-                <Link to={`/business-process/${item.id}`}>{item.name}</Link>
-              </td>
-              <td> {item.id}</td>
-              <td> {item.ancestry}</td>
-              <td>
-                <Button
-                  onClick={() => handleDelete(item.id)}
-                  color="transparent"
-                >
-                  <FaTrash />
-                </Button>
-              </td>
+    <div className="d-flex">
+      <BusinessProcessSideBox
+        searchValue={searchValue}
+        handleChange={handleChange}
+      />
+      <div className="ml-3 w-100">
+        <div className="d-flex justify-content-between align-items-center">
+          <h1>Business Process </h1>
+        </div>
+        <div>
+          <CreateBusinessProcess />
+        </div>
+        <Table reloading={businessQuery.loading}>
+          <thead>
+            <tr>
+              <th>Business Process</th>
+              <th>Business Process ID</th>
+              <th>Parent</th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {bPCollection.map(item => (
+              <tr key={item.id}>
+                <td>
+                  <Link to={`/business-process/${item.id}`}>{item.name}</Link>
+                </td>
+                <td> {item.id}</td>
+                <td> {item.ancestry}</td>
+                <td>
+                  <Button
+                    onClick={() => handleDelete(item.id)}
+                    color="transparent"
+                  >
+                    <FaTrash />
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
     </div>
   );
 };
