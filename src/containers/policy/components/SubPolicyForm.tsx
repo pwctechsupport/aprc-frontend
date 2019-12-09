485015231f5667acd655142a5876927c9f97d9d3
As'ad Saleh Umar <asad@rubyh.co>
2019-12-09 13:30:27 +0700

    Style SubPolicyForm buttons

--- src/containers/policy/components/SubPolicyForm.tsx
+++ src/containers/policy/components/SubPolicyForm.tsx
@@ -1,88 +1,92 @@
-import React, { useEffect, useState } from 'react'
-import useForm from 'react-hook-form'
-import { Form, Modal, ModalHeader, ModalBody } from 'reactstrap'
-import { oc } from 'ts-optchain'
+import React, { useEffect, useState } from "react";
+import useForm from "react-hook-form";
+import { Form, Modal, ModalHeader, ModalBody } from "reactstrap";
+import { oc } from "ts-optchain";
 import {
   useReferencesQuery,
   useResourcesQuery,
   useItSystemsQuery,
-  useBusinessProcessesQuery,
-} from '../../../generated/graphql'
-import Button from '../../../shared/components/Button'
-import Input from '../../../shared/components/forms/Input'
-import Select, { FormSelect } from '../../../shared/components/forms/Select'
-import TextEditor from '../../../shared/components/forms/TextEditor'
-import { toLabelValue } from '../../../shared/formatter'
-import LoadingSpinner from '../../../shared/components/LoadingSpinner'
+  useBusinessProcessesQuery
+} from "../../../generated/graphql";
+import Button from "../../../shared/components/Button";
+import Input from "../../../shared/components/forms/Input";
+import Select, { FormSelect } from "../../../shared/components/forms/Select";
+import TextEditor from "../../../shared/components/forms/TextEditor";
+import { toLabelValue } from "../../../shared/formatter";
+import LoadingSpinner from "../../../shared/components/LoadingSpinner";
 
-const SubPolicyForm = ({ onSubmit, defaultValues }: SubPolicyFormProps) => {
+const SubPolicyForm = ({
+  onSubmit,
+  defaultValues,
+  submitting
+}: SubPolicyFormProps) => {
   const { register, handleSubmit, setValue, errors, watch } = useForm<
     SubPolicyFormValues
   >({
-    defaultValues,
-  })
-  const [showAttrs, setShowAttrs] = useState(false)
-  const referenceData = useReferencesQuery({ variables: { filter: {} } })
+    defaultValues
+  });
+  const [showAttrs, setShowAttrs] = useState(false);
+  const referenceData = useReferencesQuery({ variables: { filter: {} } });
 
   const references = oc(referenceData)
     .data.references.collection([])
-    .map(toLabelValue)
+    .map(toLabelValue);
 
-  const resourceQ = useResourcesQuery()
+  const resourceQ = useResourcesQuery();
   const resourceOptions = oc(resourceQ.data)
     .resources.collection([])
-    .map(toLabelValue)
+    .map(toLabelValue);
 
-  const itSystemsQ = useItSystemsQuery()
+  const itSystemsQ = useItSystemsQuery();
   const itSystemsOptions = oc(itSystemsQ.data)
     .itSystems.collection([])
-    .map(toLabelValue)
+    .map(toLabelValue);
 
-  const businessProcessesQ = useBusinessProcessesQuery()
+  const businessProcessesQ = useBusinessProcessesQuery();
   const businessProcessesOptions = oc(businessProcessesQ.data)
     .businessProcesses.collection([])
-    .map(toLabelValue)
+    .map(toLabelValue);
 
   useEffect(() => {
-    register({ name: 'parentId' })
-    register({ name: 'referenceIds' })
-    register({ name: 'description', required: true })
-  }, [register])
+    register({ name: "parentId" });
+    register({ name: "referenceIds" });
+    register({ name: "description", required: true });
+  }, [register]);
 
   function handleReferenceChange(multiSelect: any) {
-    console.log('ha', multiSelect)
+    console.log("ha", multiSelect);
     if (multiSelect) {
       setValue(
-        'referenceIds',
+        "referenceIds",
         multiSelect.map((a: any) => a.value)
-      )
+      );
     }
   }
 
   function handleEditorChange(event: any, editor: any) {
-    const data = editor.getData()
-    setValue('description', data)
+    const data = editor.getData();
+    setValue("description", data);
   }
 
   function submit(values: SubPolicyFormValues) {
-    console.log('values:', values)
-    onSubmit && onSubmit(values)
-    setShowAttrs(false)
+    console.log("values:", values);
+    onSubmit && onSubmit(values);
+    setShowAttrs(false);
   }
 
   if (resourceQ.loading || referenceData.loading) {
-    return <LoadingSpinner centered />
+    return <LoadingSpinner centered />;
   }
 
   if (referenceData.loading) {
-    return <LoadingSpinner centered size={30} />
+    return <LoadingSpinner centered size={30} />;
   }
 
   const defaultReference = references.filter(reference => {
     return oc(defaultValues)
       .referenceIds([])
-      .includes(reference.value)
-  })
+      .includes(reference.value);
+  });
 
   return (
     <div>
@@ -101,7 +105,7 @@ const SubPolicyForm = ({ onSubmit, defaultValues }: SubPolicyFormProps) => {
           defaultValue={defaultReference}
         />
         <TextEditor
-          data={watch('description')}
+          data={watch("description")}
           onChange={handleEditorChange}
           invalid={errors.description ? true : false}
         />
@@ -114,7 +118,7 @@ const SubPolicyForm = ({ onSubmit, defaultValues }: SubPolicyFormProps) => {
           >
             Insert Attributes
           </Button>
-          <Button type="submit" className="pwc">
+          <Button type="submit" className="pwc px-5" loading={submitting}>
             Simpan
           </Button>
         </div>
@@ -174,36 +178,43 @@ const SubPolicyForm = ({ onSubmit, defaultValues }: SubPolicyFormProps) => {
             )}
           />
 
-          <Button
-            type="button"
-            className="mr-2"
-            onClick={() => setShowAttrs(false)}
-          >
-            Cancel
-          </Button>
+          <div className=" d-flex justify-content-end">
+            <Button
+              type="button"
+              className="mr-2 cancel"
+              onClick={() => setShowAttrs(false)}
+            >
+              Cancel
+            </Button>
 
-          <Button type="button" className="pwc" onClick={handleSubmit(submit)}>
-            Simpan
-          </Button>
+            <Button
+              type="button"
+              className="pwc px-4"
+              onClick={handleSubmit(submit)}
+            >
+              Simpan
+            </Button>
+          </div>
         </ModalBody>
       </Modal>
     </div>
-  )
-}
+  );
+};
 
-export default SubPolicyForm
+export default SubPolicyForm;
 
 export interface SubPolicyFormProps {
-  onSubmit?: (values: SubPolicyFormValues) => void
-  defaultValues: SubPolicyFormValues
+  onSubmit?: (values: SubPolicyFormValues) => void;
+  defaultValues: SubPolicyFormValues;
+  submitting?: boolean;
 }
 
 export interface SubPolicyFormValues {
-  parentId: string
-  title: string
-  description: string
-  referenceIds?: string[]
-  resourceIds?: string[]
-  itSystemIds?: string[]
-  businessProcessIds?: string[]
+  parentId: string;
+  title: string;
+  description: string;
+  referenceIds?: string[];
+  resourceIds?: string[];
+  itSystemIds?: string[];
+  businessProcessIds?: string[];
 }
