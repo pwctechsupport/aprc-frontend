25720990f08025dfa98fbee68ae5e52603f56b19
As'ad Saleh Umar <asad@rubyh.co>
2019-12-15 13:24:24 +0700

    Create reusable menu component

---
+++ src/shared/components/Menu.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+
+const Menu = ({ children, data }: MenuProps) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <Dropdown isOpen={open} toggle={() => setOpen(p => !p)}>
+      <DropdownToggle tag="div" className="clickable">
+        {children}
+      </DropdownToggle>
+      <DropdownMenu right>
+        {Array.isArray(data) &&
+          data.map(item => {
+            return (
+              <DropdownItem onClick={item.onClick}>{item.label}</DropdownItem>
+            );
+          })}
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
+
+export default Menu;
+
+// Type definitions
+interface MenuProps {
+  children: React.ReactNode;
+  data?: Array<MenuData>;
+}
+
+interface MenuData {
+  label?: string | React.ReactNode;
+  onClick?: () => void;
+}
