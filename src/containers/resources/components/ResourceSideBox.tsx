b76d5176d89b4d261f17a0079e8cb8ee2bf3bbda
As'ad Saleh Umar <asad@rubyh.co>
2019-12-16 20:39:06 +0700

    PWC-78 tambahkan added date di Resource

--- src/containers/resources/components/ResourceSideBox.tsx
+++ src/containers/resources/components/ResourceSideBox.tsx
@@ -3,13 +3,20 @@ import { Input } from "reactstrap";
 import { useResourcesQuery } from "../../../generated/graphql";
 import { oc } from "ts-optchain";
 import { Link } from "react-router-dom";
+import { date } from "../../../shared/formatter";
 
 const ResourceSideBox = () => {
   const [search, setSearch] = useState("");
   const { data } = useResourcesQuery({
     variables: { filter: { name_cont: search } }
   });
-  const resources = oc(data).resources.collection([]);
+  const resources = oc(data)
+    .resources.collection([])
+    .sort(
+      (a, b) =>
+        new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime()
+    )
+    .filter((_, i) => i < 5);
 
   return (
     <div className="side-box p-2">
@@ -19,13 +26,16 @@ const ResourceSideBox = () => {
         onChange={e => setSearch(e.target.value)}
         className="dark mb-3"
       />
+      <h5>Recently Added</h5>
       <div>
         {resources.map(resource => (
-          <div>
-            <Link className="side-box__item" to={`/resources/${resource.id}`}>
-              {resource.name}
-            </Link>
-          </div>
+          <Link
+            className="side-box__item text-white d-flex justify-content-between mt-2 py-1"
+            to={`/resources/${resource.id}`}
+          >
+            <div className="">{resource.name}</div>
+            <div>{date(resource.updatedAt)}</div>
+          </Link>
         ))}
       </div>
     </div>
