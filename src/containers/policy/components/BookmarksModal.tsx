748c76202489a9aeaef0b0603561bf402597f0dc
As'ad Saleh Umar <asad@rubyh.co>
2019-12-19 17:07:43 +0700

    Update bookmarks modal UI

--- src/containers/policy/components/BookmarksModal.tsx
+++ src/containers/policy/components/BookmarksModal.tsx
@@ -11,12 +11,21 @@ import { toggleModal } from "../../../redux/modal";
 import { useSelector } from "../../../shared/hooks/useSelector";
 import { toast } from "react-toastify";
 import { FaTrash } from "react-icons/fa";
+import LoadingSpinner from "../../../shared/components/LoadingSpinner";
 
 const BookmarksModal = () => {
   const modal = useSelector(state => state.modal.bookmark);
   const dispatch = useDispatch();
   const handleToggle = () => dispatch(toggleModal("bookmark"));
 
+  return (
+    <Modal isOpen={modal} toggle={handleToggle}>
+      <BookmarkContent />
+    </Modal>
+  );
+};
+
+const BookmarkContent = () => {
   const bookmarkPoliciesQ = useBookmarkPoliciesQuery();
   const bookmarks = oc(bookmarkPoliciesQ).data.bookmarkPolicies.collection([]);
 
@@ -27,32 +36,38 @@ const BookmarksModal = () => {
     awaitRefetchQueries: true
   });
 
+  if (bookmarkPoliciesQ.loading) return <LoadingSpinner centered size={30} />;
+
   return (
-    <Modal isOpen={modal} toggle={handleToggle}>
+    <>
       <ModalHeader>Bookmarked Policies</ModalHeader>
       <ModalBody>
         <div>
-          {bookmarks.map(bookmark => {
-            return (
-              <div
-                key={bookmark.id}
-                className="d-flex justify-content-between align-items-center py-3"
-              >
-                <Link to={`/policy/${bookmark.policyId}`}>
-                  <div>{bookmark.policyId}</div>
-                </Link>
-                <FaTrash
-                  onClick={() =>
-                    deleteBookmark({ variables: { id: bookmark.id } })
-                  }
-                  className="text-red clickable"
-                />
-              </div>
-            );
-          })}
+          {bookmarks.length === 0 ? (
+            <div className="text-center my-5">No Bookmarked Policy</div>
+          ) : (
+            bookmarks.map(bookmark => {
+              return (
+                <div
+                  key={bookmark.id}
+                  className="d-flex justify-content-between align-items-center py-3"
+                >
+                  <Link to={`/policy/${oc(bookmark).policy.id("")}`}>
+                    <div>{oc(bookmark).policy.title("")}</div>
+                  </Link>
+                  <FaTrash
+                    onClick={() =>
+                      deleteBookmark({ variables: { id: bookmark.id } })
+                    }
+                    className="text-red clickable"
+                  />
+                </div>
+              );
+            })
+          )}
         </div>
       </ModalBody>
-    </Modal>
+    </>
   );
 };
 
