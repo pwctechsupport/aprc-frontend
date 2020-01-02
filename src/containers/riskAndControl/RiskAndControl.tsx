5257d42fe324254c9925a27d4671a0bd3eeb5937
As'ad Saleh Umar <asad@rubyh.co>
2020-01-02 13:47:04 +0700

    PWC-97

--- src/containers/riskAndControl/RiskAndControl.tsx
+++ src/containers/riskAndControl/RiskAndControl.tsx
@@ -1,17 +1,33 @@
 import { capitalCase } from "capital-case";
 import get from "lodash/get";
-import React from "react";
+import React, { useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { Badge, Table } from "reactstrap";
 import { oc } from "ts-optchain";
 import { useBusinessProcessQuery } from "../../generated/graphql";
-import { UncontrolledCollapsible } from "../../shared/components/Collapsible";
+import Collapsible, {
+  UncontrolledCollapsible
+} from "../../shared/components/Collapsible";
 import EmptyAttribute from "../../shared/components/EmptyAttribute";
 import LoadingSpinner from "../../shared/components/LoadingSpinner";
 import ResourceBar from "../../shared/components/ResourceBar";
 import HeaderWithBackButton from "../../shared/components/HeaderWithBack";
+import { FaEyeSlash, FaEye } from "react-icons/fa";
+import Button from "../../shared/components/Button";
 
 const RiskAndControls = ({ match, history }: RouteComponentProps) => {
+  const initialCollapse = ["Resources", "Risks", "Controls", "Sub-Policies"];
+  const [collapse, setCollapse] = useState(initialCollapse);
+  const toggleCollapse = (name: string) =>
+    setCollapse(p => {
+      if (p.includes(name)) {
+        return p.filter(item => item !== name);
+      }
+      return p.concat(name);
+    });
+  const openAllCollapse = () => setCollapse(initialCollapse);
+  const closeAllCollapse = () => setCollapse([]);
+
   const id = get(match, "params.id", "");
   const { data, loading } = useBusinessProcessQuery({
     variables: { id },
@@ -24,10 +40,37 @@ const RiskAndControls = ({ match, history }: RouteComponentProps) => {
 
   if (loading) return <LoadingSpinner size={30} centered />;
 
+  const renderActions = () => {
+    return (
+      <Button
+        className="ml-3"
+        color="transparent"
+        onClick={() => {
+          collapse.length === initialCollapse.length
+            ? closeAllCollapse()
+            : openAllCollapse();
+        }}
+      >
+        {collapse.length === initialCollapse.length ? (
+          <FaEyeSlash size={20} />
+        ) : (
+          <FaEye size={20} />
+        )}
+      </Button>
+    );
+  };
+
   return (
     <div>
-      <HeaderWithBackButton heading={name} />
-      <UncontrolledCollapsible title="Risks">
+      <div className="d-flex justify-content-between">
+        <HeaderWithBackButton heading={name} />
+        {renderActions()}
+      </div>
+      <Collapsible
+        title="Risks"
+        show={collapse.includes("Risks")}
+        onClick={toggleCollapse}
+      >
         {risks.length ? (
           <ul>
             {risks.map(risk => (
@@ -77,8 +120,12 @@ const RiskAndControls = ({ match, history }: RouteComponentProps) => {
         ) : (
           <EmptyAttribute />
         )}
-      </UncontrolledCollapsible>
-      <UncontrolledCollapsible title="Resources">
+      </Collapsible>
+      <Collapsible
+        title="Resources"
+        show={collapse.includes("Resources")}
+        onClick={toggleCollapse}
+      >
         {resources.length ? (
           resources.map(resource => {
             return <ResourceBar key={resource.id} {...resource} />;
@@ -86,7 +133,7 @@ const RiskAndControls = ({ match, history }: RouteComponentProps) => {
         ) : (
           <EmptyAttribute />
         )}
-      </UncontrolledCollapsible>
+      </Collapsible>
     </div>
   );
 };
