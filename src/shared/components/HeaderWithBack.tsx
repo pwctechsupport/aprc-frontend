787cdc6cf481af430aedf3a8f899de166a275c53
As'ad Saleh Umar <asad@rubyh.co>
2020-01-07 17:52:02 +0700

    Style HeaderWithBack

--- src/shared/components/HeaderWithBack.tsx
+++ src/shared/components/HeaderWithBack.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import { useHistory } from "react-router-dom";
+import styled from "styled-components";
 
 function HeaderWithBackButton({ heading = "Heading" }) {
   const history = useHistory();
   const handleClick = () => history.goBack();
 
   return (
-    <div className="d-flex">
+    <div className="d-flex align-items-center">
       <div className="blue-anchor mr-3 clickable" onClick={handleClick}>
         <FaArrowLeft />
       </div>
-      &nbsp;
-      <h4 className="text-orange">{heading}</h4>
+      <Heading className="text-orange">{heading}</Heading>
     </div>
   );
 }
 
 export default HeaderWithBackButton;
+
+const Heading = styled.div`
+  font-size: 22px;
+  font-weight: 600;
+`;
