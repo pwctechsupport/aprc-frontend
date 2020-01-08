20175b7498202d917a56c3cdf50a8c0a9293d80f
As'ad Saleh Umar <asad@rubyh.co>
2020-01-09 01:53:02 +0700

    Re-style HeaderWithBack after vicinity of Breadcrumbs

--- src/shared/components/HeaderWithBack.tsx
+++ src/shared/components/HeaderWithBack.tsx
@@ -9,9 +9,9 @@ function HeaderWithBackButton({ heading = "Heading" }) {
 
   return (
     <div className="d-flex align-items-center">
-      <div className="blue-anchor mr-3 clickable" onClick={handleClick}>
+      {/* <div className="blue-anchor mr-3 clickable" onClick={handleClick}>
         <FaArrowLeft />
-      </div>
+      </div> */}
       <Heading className="text-orange">{heading}</Heading>
     </div>
   );
