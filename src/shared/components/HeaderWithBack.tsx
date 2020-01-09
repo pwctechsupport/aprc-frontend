3ba08be8a3ac63979245fe8a136062b2b20ef9e3
As'ad Saleh Umar <asad@rubyh.co>
2020-01-09 12:59:41 +0700

    Remove unused vars

--- src/shared/components/HeaderWithBack.tsx
+++ src/shared/components/HeaderWithBack.tsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { FaArrowLeft } from "react-icons/fa";
-import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 
 function HeaderWithBackButton({ heading = "Heading" }) {
-  const history = useHistory();
-  const handleClick = () => history.goBack();
-
   return (
     <div className="d-flex align-items-center">
       {/* <div className="blue-anchor mr-3 clickable" onClick={handleClick}>
