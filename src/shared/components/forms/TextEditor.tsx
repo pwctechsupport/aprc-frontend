13abc8459f7bf763452a48f2a8ec5661ab16a901
Hanggi Anggono <anggonohanggi@gmail.com>
2020-01-29 13:12:56 +0700

    Use custom text editor toolbar

--- src/shared/components/forms/TextEditor.tsx
+++ src/shared/components/forms/TextEditor.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import classnames from 'classnames'
-import Quill from 'react-quill'
+import ReactQuill, { Quill } from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 
 export interface TextEditorProps {
@@ -9,6 +9,62 @@ export interface TextEditorProps {
   onChange?: (content: string) => void
   invalid?: boolean
 }
+
+const fontSize = Quill.import('attributors/style/size')
+const defaultFontSize = 13
+const fontSizes = [16, 18, 20, 24, 30]
+fontSize.whitelist = [`${defaultFontSize}px`, ...fontSizes.map(s => `${s}px`)]
+Quill.register(fontSize, true)
+
+const QuillCustomToolbar = () => (
+  <div id="toolbar">
+    <span className="ql-formats">
+      <select className="ql-font"></select>
+    </span>
+    <span className="ql-formats">
+      <select className="ql-header" defaultValue={""} onChange={e => e.persist()}>
+        <option value="1"></option>
+        <option value="2"></option>
+        <option selected></option>
+      </select>
+    </span>
+    <span className="ql-formats">
+      <select className="ql-size">
+        <option selected>{defaultFontSize}</option>
+        {fontSizes.map(size => {
+          return (
+            <option value={`${size}px`}>{size}</option>
+          )
+        })}
+      </select>
+    </span>
+    <span className="ql-formats">
+      <button className="ql-bold"></button>
+      <button className="ql-italic"></button>
+      <button className="ql-underline"></button>
+      <button className="ql-strike"></button>
+      <button className="ql-blockquote"></button>
+    </span>
+    <span className="ql-formats">
+      <select className="ql-color"></select>
+      <select className="ql-background"></select>
+    </span>
+    <span className="ql-formats">
+      <select className="ql-align"></select>
+      <button className="ql-list" value="ordered"></button>
+      <button className="ql-list" value="bullet"></button>
+      <button className="ql-indent" value="-1"></button>
+      <button className="ql-indent" value="+1"></button>
+    </span>
+    <span className="ql-formats">
+      <button className="ql-link"></button>
+      <button className="ql-image"></button>
+    </span>
+    <span className="ql-formats">
+      <button className="ql-clean"></button>
+    </span>
+  </div>
+)
 
 const TextEditor = ({
   data,
@@ -24,28 +80,18 @@ const TextEditor = ({
         invalid && 'invalid',
       ])}
     >
-      <Quill
+      <QuillCustomToolbar/>
+      <ReactQuill
         value={data}
         onChange={onChange}
         modules={{
-          toolbar: [
-            [{ header: [1, 2, false] }],
-            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-            [{color: []}, {background: []}],
-            [{ 'font': [] }],
-            [{ 'align': [] }],
-            [
-              { list: 'ordered' },
-              { list: 'bullet' },
-              { indent: '-1' },
-              { indent: '+1' },
-            ],
-            ['link', 'image'],
-            ['clean'],
-          ],
+          toolbar: {
+            container: '#toolbar'
+          },
         }}
         formats={[
           'header',
+          'size',
           'bold',
           'italic',
           'underline',
