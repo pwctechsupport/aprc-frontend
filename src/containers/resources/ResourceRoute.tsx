b0032c04be01a41b27e318d4f08d1f32a75c6653
Gilang Faras Arifah <gilang.f@Gilangs-MacBook-Pro.local>
2020-01-31 18:38:16 +0700

    Fix view

--- src/containers/resources/ResourceRoute.tsx
+++ src/containers/resources/ResourceRoute.tsx
@@ -4,22 +4,24 @@ import Resources from "./Resources";
 import Resource from "./Resource";
 import CreateResource from "./CreateResource";
 import ResourceSideBox from "./components/ResourceSideBox";
-import { Row, Col } from "reactstrap";
+import { Row, Col, Container } from "reactstrap";
 
 const ResourceRoute = () => {
   return (
-    <Row>
-      <Col md={3}>
-        <Route path="/resources" component={ResourceSideBox} />
-      </Col>
-      <Col md={9}>
-        <Route exact path="/resources" component={Resources} />
-        <Switch>
-          <Route exact path="/resources/create" component={CreateResource} />
-          <Route exact path="/resources/:id" component={Resource} />
-        </Switch>
-      </Col>
-    </Row>
+    <Container fluid className="p-0">
+      <Row noGutters>
+        <Col md={3}>
+          <Route path="/resources" component={ResourceSideBox} />
+        </Col>
+        <Col md={9} className="p-4">
+          <Route exact path="/resources" component={Resources} />
+          <Switch>
+            <Route exact path="/resources/create" component={CreateResource} />
+            <Route exact path="/resources/:id" component={Resource} />
+          </Switch>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
