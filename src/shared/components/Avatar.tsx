afa5c6ee4a4ed32d42f2ec51b243aef39ba42483
Gilang Faras Arifah <gilang.f@Gilangs-MacBook-Pro.local>
2020-02-03 16:43:42 +0700

    Avatar dropdown

--- src/shared/components/Avatar.tsx
+++ src/shared/components/Avatar.tsx
@@ -7,9 +7,11 @@ import {
 } from "reactstrap";
 import styled from "styled-components";
 import Button from "./Button";
+import { useSelector } from "../hooks/useSelector";
 
 const Avatar = ({ data }: AvatarProps) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const user = useSelector(state => state.auth.user);
 
   const toggle = () => setDropdownOpen(prevState => !prevState);
 
@@ -22,11 +24,32 @@ const Avatar = ({ data }: AvatarProps) => {
         {data.map((item, index) => (
           <div
             key={index}
-            className="d-flex flex-column justify-content-center p-4"
+            className="d-flex flex-column p-4"
+            style={{ width: 260 }}
           >
-            <AvatarIcon src="https://reactnativecode.com/wp-content/uploads/2018/01/2_img.png" />
-            <h4>Nama</h4>
-            <Button onClick={item.onClick}>Log out</Button>
+            <div className="d-flex flex-column justify-content-center align-items-center mb-3">
+              <DropdownAvatarIcon src="https://reactnativecode.com/wp-content/uploads/2018/01/2_img.png" />
+              {user ? <h4>{user.name}</h4> : null}
+            </div>
+            <div className="d-flex justify-content-between">
+              {user ? (
+                <>
+                  <UserDetailTitle>Email</UserDetailTitle>
+                  <UserDetail>{user.email}</UserDetail>
+                </>
+              ) : null}
+            </div>
+            <div className="d-flex justify-content-between">
+              {user ? (
+                <>
+                  <UserDetailTitle>Phone</UserDetailTitle>
+                  <UserDetail>{user.phone}</UserDetail>
+                </>
+              ) : null}
+            </div>
+            <Button onClick={item.onClick} color="primary" className="pwc mt-4">
+              Log out
+            </Button>
           </div>
         ))}
       </DropdownMenu>
@@ -44,6 +67,24 @@ const AvatarIcon = styled.img`
   width: 44px;
   border-radius: 50%;
   cursor: pointer;
+`;
+
+const DropdownAvatarIcon = styled.img`
+  display: inline-block;
+  position: relative;
+  outline: 0px;
+  height: 80px;
+  width: 80px;
+  border-radius: 50%;
+`;
+
+const UserDetailTitle = styled.div`
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+const UserDetail = styled.div`
+  font-size: 14px;
 `;
 
 // -------------------------------------------------
