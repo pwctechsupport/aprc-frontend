8fac7f38d4ef20e9771241a334d1733686002839
Gilang Faras Arifah <gilang.f@Gilangs-MacBook-Pro.local>
2020-02-05 19:01:15 +0700

    history page

--- src/containers/settings/SettingsRoute.tsx
+++ src/containers/settings/SettingsRoute.tsx
@@ -15,9 +15,13 @@ const SettingsRoute = () => {
           </Col>
           <Col md={9} className="p-4">
             <Switch>
-              <Route exact path="/settings/profile" component={UpdateProfile} />
+              <Route
+                exact
+                path="/settings/update-profile"
+                component={UpdateProfile}
+              />
               <Route exact path="/settings/history" component={History} />
-              <Redirect exact from="/settings" to="/settings/profile" />
+              <Redirect exact from="/settings" to="/settings/update-profile" />
             </Switch>
           </Col>
         </Row>
