6cf679378646952966a7c80af39efe1b6f7c44a4
Gilang Faras Arifah <gilang.f@Gilangs-MacBook-Pro.local>
2020-02-10 17:32:05 +0700

    working isRead notification and search notification

--- src/containers/notification/Notification.tsx
+++ src/containers/notification/Notification.tsx
@@ -16,32 +16,36 @@ import {
 import LoadingSpinner from "../../shared/components/LoadingSpinner";
 import { RouteComponentProps } from "react-router";
 import { toast } from "react-toastify";
+import { oc } from "ts-optchain";
+import { NetworkStatus } from "apollo-boost";
+import { useDebounce } from "use-debounce/lib";
 
-const dummy = [
-  {
-    id: 0,
-    createdAt: new Date(),
-    isRead: false,
-    title: "",
-    originatorType: "",
-    originatorId: 0,
-    selected: false,
-    senderUser: {
-      name: "",
-      firstName: "",
-      lastName: ""
-    }
-  }
-];
 const Notification = ({ history }: RouteComponentProps) => {
-  const { data, loading } = useNotificationsQuery({
-    fetchPolicy: "network-only"
-  });
+  const [selected, setSelected] = useState<string[]>([]);
   const [search, setSearch] = useState("");
-  const [selectAll, setSelectAll] = useState(false);
-  const [notifs, setNotifs] = useState(dummy);
+  const [debounceSearch] = useDebounce(search, 800);
+
+  const { data, networkStatus } = useNotificationsQuery({
+    fetchPolicy: "network-only",
+    variables: {
+      filter: {
+        title_or_originator_type_cont: debounceSearch
+        // originator_of_Policy_type_title_or_originator_of_BusinessProcess_type_name_or_originator_of_Control_type_description_or_originator_of_Risk_type_name_or_originator_of_User_type_name_cont: debounceSearch
+      }
+    }
+  });
+  const notifications = oc(data)
+    .notifications.collection([])
+    .sort(
+      (a, b) =>
+        new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+    );
+
   const [destroyNotifs] = useDestroyBulkNotificationMutation({
-    onCompleted: () => toast.success("Delete Success"),
+    onCompleted: () => {
+      toast.success("Delete Success");
+      onDeleteComplete();
+    },
     onError: () => toast.error("Delete Failed"),
     refetchQueries: ["notifications"],
     awaitRefetchQueries: true
@@ -50,67 +54,14 @@ const Notification = ({ history }: RouteComponentProps) => {
     refetchQueries: ["notifications"],
     awaitRefetchQueries: true
   });
-  const setItemChecked = (id: string, e: ChangeEvent) => {
-    setNotifs(n =>
-      n.map(item =>
-        String(item.id) === id ? { ...item, selected: !item.selected } : item
-      )
-    );
-    checkSelection();
-    e.stopPropagation();
-  };
 
-  const prepareNotifs = () => {
-    const prepared: any[] = [];
-    if (data) {
-      const n = data.notifications;
-      const notifs = n ? n.collection : [];
-      if (notifs) {
-        notifs.map(n => {
-          const temp = {
-            ...n,
-            selected: false
-          };
-          prepared.push(temp);
-        });
-        setNotifs(prepared);
-      }
-    }
-  };
-
-  useEffect(() => {
-    prepareNotifs();
-  }, [data]);
-
-  useEffect(() => {
-    checkSelection();
-  }, [notifs]);
-
-  const checkSelection = () => {
-    const tmp = notifs.filter(n => n.selected);
-    console.log("all", selectAll);
-    console.log("checked", tmp);
-    if (tmp.length == notifs.length) {
-      setSelectAll(!selectAll);
-    }
-    console.log("aftercheck", selectAll);
-  };
-
-  const setSelectAllCheck = () => {
-    if (selectAll) {
-      setNotifs(n => n.map(item => ({ ...item, selected: false })));
-      setSelectAll(!selectAll);
-    } else {
-      setNotifs(n => n.map(item => ({ ...item, selected: true })));
-    }
-  };
-
-  const redirect = (type: String, id: number) => {
-    const input: IsReadInput = { id: String(id) };
-    console.log("i", input);
+  const redirect = (type: String, id: number, notifId: String) => {
+    const readId: IsReadInput = { id: String(notifId) };
+    console.log("i", readId);
     isRead({
-      variables: { input }
+      variables: { input: readId }
     });
+
     if (type == "Policy") history.push(`/policy/${id}`);
     else if (type == "BusinessProcess") history.push(`/business-process/${id}`);
     else if (type == "Control") history.push(`/control/${id}`);
@@ -119,17 +70,31 @@ const Notification = ({ history }: RouteComponentProps) => {
   };
 
   const handleDelete = () => {
-    const toBeDeleted = notifs.filter(n => n.selected);
-    const temp: string[] = [];
-    toBeDeleted.map(data => {
-      const notifsId = String(data.id);
-      temp.push(notifsId);
-    });
-    const notifIds: DestroyBulkNotificationInput = { ids: temp };
+    const notifIds: DestroyBulkNotificationInput = { ids: selected };
     destroyNotifs({
       variables: { input: notifIds }
     });
   };
+
+  function onDeleteComplete() {
+    setSelected([]);
+  }
+
+  function toggleCheck(id: string) {
+    if (selected.includes(id)) {
+      setSelected(selected.filter(i => i !== id));
+    } else {
+      setSelected(selected.concat(id));
+    }
+  }
+
+  function toggleCheckAll(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.target.checked) {
+      setSelected(notifications.map(n => String(n.id)));
+    } else {
+      setSelected([]);
+    }
+  }
 
   return (
     <div>
@@ -145,7 +110,7 @@ const Notification = ({ history }: RouteComponentProps) => {
             />
           </div>
           <DialogButton onConfirm={() => handleDelete()}>
-            <Tooltip description="Delete Selected History">
+            <Tooltip description="Delete Selected Notification(s)">
               <div className="clickable d-flex justify-content-center align-items-center deleteButton">
                 <FaTrash className="text-orange " size={22} />
               </div>
@@ -153,44 +118,57 @@ const Notification = ({ history }: RouteComponentProps) => {
           </DialogButton>
         </div>
         <div className="table-responsive mt-5">
-          {loading ? (
-            <LoadingSpinner centered size={30} />
-          ) : (
-            <Table>
-              <thead>
-                <tr>
-                  <th>
-                    <input
-                      type="checkbox"
-                      checked={selectAll}
-                      onChange={e => setSelectAllCheck()}
-                    />
-                  </th>
-                  <th>Name</th>
-                  <th>Subject</th>
-                  <th>Date Added</th>
-                </tr>
-              </thead>
-              <tbody>
-                {notifs.map(data => (
+          <Table
+            loading={networkStatus === NetworkStatus.loading}
+            reloading={networkStatus === NetworkStatus.setVariables}
+          >
+            <thead>
+              <tr>
+                <th>
+                  <input
+                    type="checkbox"
+                    checked={
+                      selected.length === notifications.length ? true : false
+                    }
+                    onChange={toggleCheckAll}
+                  />
+                </th>
+                <th>Name</th>
+                <th>Subject</th>
+                <th>Date Added</th>
+              </tr>
+            </thead>
+            <tbody>
+              {notifications.map(data =>
+                data ? (
                   <tr
-                    key={data.id}
+                    key={String(data.id)}
                     onClick={() =>
-                      redirect(data.originatorType, data.originatorId)
+                      data.originatorType
+                        ? redirect(
+                            data.originatorType,
+                            Number(data.originatorId),
+                            String(data.id)
+                          )
+                        : null
                     }
                   >
                     <td>
                       <input
                         type="checkbox"
-                        checked={data.selected}
-                        onChange={e => setItemChecked(String(data.id), e)}
+                        checked={selected.includes(String(data.id))}
+                        onClick={e => e.stopPropagation()}
+                        onChange={e => toggleCheck(String(data.id))}
                       />
                     </td>
                     <td className={data.isRead ? "" : "text-orange text-bold"}>
-                      {data.senderUser.name}
+                      {data.senderUser ? data.senderUser.name : ""}
                     </td>
                     <td className={data.isRead ? "" : "text-orange text-bold"}>
-                      {data.title}
+                      {"Request to edit " +
+                        data.originatorType +
+                        " : " +
+                        data.title}
                     </td>
                     <td className={data.isRead ? "" : "text-orang text-bold"}>
                       {formatDate(data.createdAt, {
@@ -200,10 +178,10 @@ const Notification = ({ history }: RouteComponentProps) => {
                       })}
                     </td>
                   </tr>
-                ))}
-              </tbody>
-            </Table>
-          )}
+                ) : null
+              )}
+            </tbody>
+          </Table>
         </div>
       </Container>
     </div>
