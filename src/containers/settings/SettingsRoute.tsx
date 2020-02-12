c37b472de54db506f61345037b4d5eb908bc5e11
As'ad Saleh Umar <asad@rubyh.co>
2020-02-12 13:23:33 +0700

    Fix: Delete unncessary Switch

--- src/containers/settings/SettingsRoute.tsx
+++ src/containers/settings/SettingsRoute.tsx
@@ -8,31 +8,29 @@ import SettingsSideBox from "./components/SettingsSideBox";
 
 const SettingsRoute = () => {
   return (
-    <Switch>
-      <Container fluid className="p-0">
-        <Row noGutters>
-          <Col md={3}>
-            <Route path="/settings" component={SettingsSideBox} />
-          </Col>
-          <Col md={9} className="p-4">
-            <Switch>
-              <Route
-                exact
-                path="/settings/update-profile"
-                component={UpdateProfile}
-              />
-              <Route exact path="/settings/history" component={History} />
-              <Route
-                exact
-                path="/settings/notifications"
-                component={NotificationSettings}
-              />
-              <Redirect exact from="/settings" to="/settings/update-profile" />
-            </Switch>
-          </Col>
-        </Row>
-      </Container>
-    </Switch>
+    <Container fluid className="p-0">
+      <Row noGutters>
+        <Col md={3}>
+          <Route path="/settings" component={SettingsSideBox} />
+        </Col>
+        <Col md={9} className="p-4">
+          <Switch>
+            <Route
+              exact
+              path="/settings/update-profile"
+              component={UpdateProfile}
+            />
+            <Route exact path="/settings/history" component={History} />
+            <Route
+              exact
+              path="/settings/notifications"
+              component={NotificationSettings}
+            />
+            <Redirect exact from="/settings" to="/settings/update-profile" />
+          </Switch>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
