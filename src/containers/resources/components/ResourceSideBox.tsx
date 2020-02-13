103ee719f7a6853593bf20da3555dd3b9a083204
As'ad Saleh Umar <asad@rubyh.co>
2020-02-13 15:20:13 +0700

    Refactor: Use new SideBox in Resource

--- src/containers/resources/components/ResourceSideBox.tsx
+++ src/containers/resources/components/ResourceSideBox.tsx
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
-import { Input } from "reactstrap";
-import { useResourcesQuery } from "../../../generated/graphql";
 import { oc } from "ts-optchain";
-import { Link } from "react-router-dom";
-import humanizeDate from "../../../shared/utils/humanizeDate";
 import { useDebounce } from "use-debounce/lib";
+import { useResourcesQuery } from "../../../generated/graphql";
+import {
+  SideBox,
+  SideBoxItem,
+  SideBoxItemText,
+  SideBoxSearch,
+  SideBoxTitle
+} from "../../../shared/components/SideBox";
+import humanizeDate from "../../../shared/utils/humanizeDate";
 
 const ResourceSideBox = () => {
   const [search, setSearch] = useState("");
-  const [searchQuery] = useDebounce(search, 500);
-  const { data } = useResourcesQuery({
+  const [searchQuery] = useDebounce(search, 400);
+  const { data, loading } = useResourcesQuery({
     variables: { filter: { name_cont: searchQuery } }
   });
   const resources = oc(data)
@@ -20,29 +25,25 @@ const ResourceSideBox = () => {
     );
 
   return (
-    <div className="side-box p-3">
-      <Input
-        value={search}
+    <SideBox>
+      <SideBoxTitle>Recently Added</SideBoxTitle>
+      <SideBoxSearch
+        search={search}
+        setSearch={setSearch}
         placeholder="Search Resources..."
-        onChange={e => setSearch(e.target.value)}
-        className="orange mb-3"
+        loading={loading}
       />
-      <h5 className="text-orange">Recently Added</h5>
-      <div>
-        {resources.map(resource => (
-          <Link
-            className="side-box__item text-white d-flex justify-content-between mt-2 py-1"
-            to={`/resources/${resource.id}`}
-            key={resource.id}
-          >
-            <div className="text-orange">{resource.name}</div>
-            <div className="text-orange">
-              {humanizeDate(new Date(resource.updatedAt))}
-            </div>
-          </Link>
-        ))}
-      </div>
-    </div>
+      {resources.map(resource => (
+        <SideBoxItem key={resource.id} to={`/resources/${resource.id}`}>
+          <SideBoxItemText flex={2} bold>
+            {resource.name}
+          </SideBoxItemText>
+          <SideBoxItemText flex={1} right>
+            {humanizeDate(new Date(resource.updatedAt))}
+          </SideBoxItemText>
+        </SideBoxItem>
+      ))}
+    </SideBox>
   );
 };
 
