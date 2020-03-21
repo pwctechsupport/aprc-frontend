c17d3ee60d21968890e0603ee961619726db97fe
As'ad Saleh Umar <asad@rubyh.co>
2020-03-22 00:34:03 +0700

    Feature: PWC-292 | Create homepage search

---
+++ src/containers/homepage/HomepageSearch.tsx
@@ -0,0 +1,129 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { components } from "react-select";
+import Select from "react-select/async";
+import { Badge, Container } from "reactstrap";
+import styled from "styled-components";
+import {
+  PoliciesDocument,
+  PoliciesQuery,
+  Policy
+} from "../../generated/graphql";
+import { previewHtml } from "../../shared/formatter";
+import useLazyQueryReturnPromise from "../../shared/hooks/useLazyQueryReturnPromise";
+
+export default function HomepageSearch() {
+  const loadPolicies = useLoadPolicies();
+  return (
+    <Centerer>
+      <Container>
+        <Select
+          placeholder="Search Policy..."
+          components={{ Option }}
+          loadOptions={loadPolicies}
+          defaultOptions
+        />
+      </Container>
+    </Centerer>
+  );
+}
+
+const Option = (props: any) => {
+  const id = props?.data?.id;
+  const title = props?.data?.title;
+  const description = props?.data?.description;
+  const risks = props?.data?.risks || [];
+  const controls = props?.data?.controls || [];
+  const references = props?.data?.references || [];
+  return (
+    <StyledLink to={`/policy/${id}`}>
+      <components.Option {...props}>
+        <h5>{title}</h5>
+        <PreviewSpan>{previewHtml(description, 200)}</PreviewSpan>
+        <div>
+          {references?.map((reference: any) => (
+            <Badge key={reference.id} className="mx-1">
+              {reference.name}
+            </Badge>
+          ))}
+        </div>
+        <div>
+          {risks.length ? (
+            <div>
+              <CompressedText>Risks:</CompressedText>
+              <SmallerPreviewSpan>
+                {" "}
+                {risks.map((risk: any) => risk.name).join(", ")}
+              </SmallerPreviewSpan>
+            </div>
+          ) : null}
+        </div>
+        <div>
+          {controls.length ? (
+            <div>
+              <CompressedText>Controls:</CompressedText>
+              <SmallerPreviewSpan>
+                {controls.map((control: any) => control.description).join(", ")}
+              </SmallerPreviewSpan>
+            </div>
+          ) : null}
+        </div>
+      </components.Option>
+    </StyledLink>
+  );
+};
+
+// =============================================
+// Styled Components
+// =============================================
+
+const Centerer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
+
+const PreviewSpan = styled.span`
+  color: rgba(0, 0, 0, 0.5);
+`;
+
+const SmallerPreviewSpan = styled(PreviewSpan)`
+  font-size: smaller;
+`;
+
+const StyledLink = styled(Link)`
+  color: unset;
+  &:hover {
+    text-decoration: none;
+  }
+`;
+
+const CompressedText = styled.span`
+  letter-spacing: 1.5px;
+  font-size: smaller;
+`;
+
+// =============================================
+// Custom Hooks
+// =============================================
+
+type MyPolicy = Omit<Policy, "createdAt" | "updatedAt">;
+
+function useLoadPolicies() {
+  const query = useLazyQueryReturnPromise<PoliciesQuery>(PoliciesDocument);
+  async function getSuggestions(keyword: string = ""): Promise<MyPolicy[]> {
+    try {
+      const { data } = await query({
+        filter: {
+          references_name_or_risks_name_or_controls_description_or_title_or_description_cont: keyword
+        },
+        withDetail: true
+      });
+      return data.policies?.collection || [];
+    } catch (error) {
+      return [];
+    }
+  }
+  return getSuggestions;
+}
