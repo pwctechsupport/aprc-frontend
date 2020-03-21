7ac52c1a95a8fb44e41360043d30714a0f2f5bf7
As'ad Saleh Umar <asad@rubyh.co>
2020-03-21 22:38:28 +0700

    Style HomepageBox

--- src/containers/homepage/HomepageBox.tsx
+++ src/containers/homepage/HomepageBox.tsx
@@ -20,24 +20,25 @@ export default function HomepageBox({
   return (
     <div className="my-2">
       <Card className="p-3 box-shadow">
-        <div
-          className="d-flex justify-content-between align-items-center"
-          onClick={() => setOpen(p => !p)}
-        >
+        <div className="d-flex justify-content-between align-items-center">
           <h5>{title}</h5>
-          <BoxHeader>
+          <BoxHeader onClick={() => setOpen(p => !p)}>
             <Icon open={open} />
           </BoxHeader>
         </div>
         <Collapse isOpen={open}>
           <div>
             {list.map(item => (
-              <div key={item.value} className="d-flex align-items-center my-2">
+              <StyledLink
+                key={item.value}
+                to={`/${basePath}/${item.value}`}
+                className="d-flex align-items-center my-2"
+              >
                 <div className="mr-3">
                   <Circle>{item.label.charAt(0).toUpperCase()}</Circle>
                 </div>
-                <Link to={`/${basePath}/${item.value}`}>{item.label}</Link>
-              </div>
+                {item.label}
+              </StyledLink>
             ))}
           </div>
         </Collapse>
@@ -45,6 +46,17 @@ export default function HomepageBox({
     </div>
   );
 }
+
+const StyledLink = styled(Link)`
+  color: rgba(0, 0, 0, 0.5);
+  font-size: larger;
+  border-radius: 5px;
+  transition: 0.1s ease-in-out;
+  &:hover {
+    text-decoration: none;
+    color: rgba(0, 0, 0, 0.8);
+  }
+`;
 
 const Circle = styled.div`
   width: 30px;
@@ -54,6 +66,10 @@ const Circle = styled.div`
   color: #fff;
   text-align: center;
   background: rgba(0, 0, 0, 0.4);
+  transition: 0.1s ease-in-out;
+  ${StyledLink}:hover & {
+    background: rgba(0, 0, 0, 0.8);
+  }
 `;
 
 const BoxHeader = styled.div`
