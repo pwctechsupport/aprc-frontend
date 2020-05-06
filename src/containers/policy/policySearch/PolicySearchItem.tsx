cdb309838ddeb030725a935f00f3eace49efeba6
As'ad Saleh Umar <asad@rubyh.co>
2020-05-06 11:07:32 +0700

    WIP PWC-392

--- src/containers/policy/policySearch/PolicySearchItem.tsx
+++ src/containers/policy/policySearch/PolicySearchItem.tsx
@@ -2,16 +2,19 @@ import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { Policy } from "../../../generated/graphql";
-import EmptyAttribute from "../../../shared/components/EmptyAttribute";
+// import EmptyAttribute from "../../../shared/components/EmptyAttribute";
 import { previewHtml } from "../../../shared/formatter";
 import DateHover from "../../../shared/components/DateHover";
 import { Row, Col } from "reactstrap";
 
 interface PolicySearchItemProps {
   policy: Omit<Policy, "createdAt">;
-  homepageSearch:string;
+  homepageSearch: string;
 }
-export default function PolicySearchItem({ policy, homepageSearch }: PolicySearchItemProps) {
+export default function PolicySearchItem({
+  policy,
+  homepageSearch,
+}: PolicySearchItemProps) {
   const {
     id,
     title,
@@ -20,199 +23,227 @@ export default function PolicySearchItem({ policy, homepageSearch }: PolicySearc
     controls,
     resources,
     references,
-    updatedAt
+    updatedAt,
   } = policy;
 
-  const noAttribute =
-    risks?.length === 0 &&
-    controls?.length === 0 &&
-    resources?.length === 0 &&
-    references?.length === 0;
+  // const noAttribute =
+  //   risks?.length === 0 &&
+  //   controls?.length === 0 &&
+  //   resources?.length === 0 &&
+  //   references?.length === 0;
 
-const showResource= resources?.map(a=>a.name?.toLowerCase().includes(homepageSearch.toLowerCase()))
-const showReference= references?.map(a=>a.name?.toLowerCase().includes(homepageSearch.toLowerCase()))
-const showControl= controls?.map(a=>a.description?.toLowerCase().includes(homepageSearch.toLowerCase()))
-const showRisk= risks?.map(a=>a.name?.toLowerCase().includes(homepageSearch.toLowerCase()))
+  const showResource = resources?.map((a) =>
+    a.name?.toLowerCase().includes(homepageSearch.toLowerCase())
+  );
+  const showReference = references?.map((a) =>
+    a.name?.toLowerCase().includes(homepageSearch.toLowerCase())
+  );
+  const showControl = controls?.map((a) =>
+    a.description?.toLowerCase().includes(homepageSearch.toLowerCase())
+  );
+  const showRisk = risks?.map((a) =>
+    a.name?.toLowerCase().includes(homepageSearch.toLowerCase())
+  );
 
   return (
     <Fragment>
       {/* homepageSearch  is a string for search */}
-      {homepageSearch!=='thisIsForSearchPolicy'?
-        <PolicySearchItemContainerMini>
-          <Row>
+      <PolicySearchItemContainerMini>
+        <Row>
           <StyledLink to={`/policy/${id}/details`}>
-              <div className='ml-2'>
-                <StyledTitle>
-                  <strong>{title}</strong>
-                </StyledTitle>
-              </div>
-              <div className='mb-2 ml-2'>
-                <DateHover withIcon>{updatedAt}</DateHover>
-              </div>
-              <div className="text-secondary ml-2">{previewHtml(description || "")}</div>
-            </StyledLink>
-            </Row>
-          <Row style={{marginLeft:'1vw',marginRight:'1vw'}}
-          >
-            {!showResource?.every(a=>a===false) && homepageSearch!=="" && (
-            <Col md={3}>
-              <StyledLi>
-                <Names>Resources:</Names>
-                <ul>
-                  {resources?.map(resource => (
-                    resource?.name?.toLowerCase().includes(homepageSearch.toLowerCase()) &&
-                    <li>
-                    <StyledTdMini key={resource.id}>
-                      <Link to={`/policy/${id}/resources`}>
-                        {resource.name}
-                      </Link>
-                    </StyledTdMini>
-                    </li>
-                ))}
-                </ul>
-              </StyledLi>
-              </Col>
-            )}
-              {!showReference?.every(a=>a===false) && homepageSearch!=="" && (
-            <Col md={3}>
-                <StyledLi>
-                  <Names>References:</Names>
-                  <ul>
-                    {references?.map(reference => (
-                      reference?.name?.toLowerCase().includes(homepageSearch.toLowerCase()) &&
-                    <li>
-                      <StyledTdMini key={reference.id}>
-                        <Link to={`/policy/${id}/details/#references`}>
-                          {reference.name}
-                        </Link>
-                      </StyledTdMini>
-                    </li>
-                    ))}
-                  </ul>
-                </StyledLi>
-            </Col>
-              )}
-              {!showRisk?.every(a=>a===false) && homepageSearch!=="" && (
-            <Col md={3}>
-                  <StyledLi>
-                    <Names> Risks</Names>
-                    <ul>
-                      {risks?.map(risk => (
-                      risk?.name?.toLowerCase().includes(homepageSearch.toLowerCase()) &&
-                    <li>
-                        <StyledTdMini key={risk.id}>  
-                          <Link to={`/policy/${id}/details/#risks`}>
-                            {risk.name}
-                          </Link>
-                        </StyledTdMini>
-                    </li>
-                      ))}
-                    </ul>
-                  </StyledLi>
-            </Col>
-                ) }
-                {!showControl?.every(a=>a===false) && homepageSearch!=="" && (
-            <Col md={3}>
-                  <StyledLi>
-                    <Names>Controls:</Names>
-                    <ul>
-                      {controls?.map(control => (
-                      control?.description?.toLowerCase().includes(homepageSearch.toLowerCase()) &&                    
-                      <li>
-                        <StyledTdMini key={control.id}>
-                          <Link to={`/policy/${id}/details/#controls`}>
-                            {control.description}
-                          </Link>
-                        </StyledTdMini>
-                      </li>
-                      ))}
-                    </ul>
-                  </StyledLi>
-            </Col>
-                )}
-          </Row>
-
-        </PolicySearchItemContainerMini>
-      :
-      <PolicySearchItemContainer>
-        <AttributeSection>
-          {noAttribute ? (
-            <EmptyAttribute>No Attribute</EmptyAttribute>
-          ) : (
-            <StyledUl>
-              {risks?.length ? (
-                <StyledLi>
-                  <Names> Risks</Names>
-                  <ul>
-                    {risks?.map(risk => (
-                      <StyledTd key={risk.id}>
-                        <Link to={`/policy/${id}/details/#risks`}>
-                          {risk.name}
-                        </Link>
-                      </StyledTd>
-                    ))}
-                  </ul>
-                </StyledLi>
-              ) : null}
-              {controls?.length ? (
-                <StyledLi>
-                  <Names>Controls:</Names>
-                  <ul>
-                    {controls?.map(control => (
-                      <StyledTd key={control.id}>
-                        <Link to={`/policy/${id}/details/#controls`}>
-                          {control.description}
-                        </Link>
-                      </StyledTd>
-                    ))}
-                  </ul>
-                </StyledLi>
-              ) : null}
-              {resources?.length ? (
-                <StyledLi>
-                  <Names>Resources:</Names>
-                  <ul>
-                    {resources?.map(resource => (
-                      <StyledTd key={resource.id}>
-                        <Link to={`/policy/${id}/resources`}>
-                          {resource.name}
-                        </Link>
-                      </StyledTd>
-                    ))}
-                  </ul>
-                </StyledLi>
-              ) : null}
-              {references?.length ? (
-                <StyledLi>
-                  <Names>References:</Names>
-                  <ul>
-                    {references?.map(reference => (
-                      <StyledTd key={reference.id}>
-                        <Link to={`/policy/${id}/details/#references`}>
-                          {reference.name}
-                        </Link>
-                      </StyledTd>
-                    ))}
-                  </ul>
-                </StyledLi>
-              ) : null}
-            </StyledUl>
-          )}
-        </AttributeSection>
-        <TitleAndDescriptionSection>
-          <StyledLink to={`/policy/${id}/details`}>
-            <div className="text-right">
-              <DateHover withIcon>{updatedAt}</DateHover>
-            </div>
-            <div>
+            <div className="ml-2">
               <StyledTitle>
                 <strong>{title}</strong>
               </StyledTitle>
             </div>
-            <div className="text-secondary">{previewHtml(description || "")}</div>
+            <div className="mb-2 ml-2">
+              <DateHover withIcon>{updatedAt}</DateHover>
+            </div>
+            <div className="text-secondary ml-2">
+              {description
+                ?.toLowerCase()
+                .includes(homepageSearch.toLowerCase()) &&
+                previewHtml(description || "")}
+            </div>
           </StyledLink>
-        </TitleAndDescriptionSection>
-      </PolicySearchItemContainer>}
+        </Row>
+        <Row style={{ marginLeft: "1vw", marginRight: "1vw" }}>
+          {!showResource?.every((a) => a === false) && homepageSearch !== "" && (
+            <Col md={3}>
+              <StyledLi>
+                <Names>Resources:</Names>
+                <ul>
+                  {resources?.map(
+                    (resource) =>
+                      resource?.name
+                        ?.toLowerCase()
+                        .includes(homepageSearch.toLowerCase()) && (
+                        <li>
+                          <StyledTdMini key={resource.id}>
+                            <Link to={`/policy/${id}/resources`}>
+                              {resource.name}
+                            </Link>
+                          </StyledTdMini>
+                        </li>
+                      )
+                  )}
+                </ul>
+              </StyledLi>
+            </Col>
+          )}
+          {!showReference?.every((a) => a === false) && homepageSearch !== "" && (
+            <Col md={3}>
+              <StyledLi>
+                <Names>References:</Names>
+                <ul>
+                  {references?.map(
+                    (reference) =>
+                      reference?.name
+                        ?.toLowerCase()
+                        .includes(homepageSearch.toLowerCase()) && (
+                        <li>
+                          <StyledTdMini key={reference.id}>
+                            <Link to={`/policy/${id}/details/#references`}>
+                              {reference.name}
+                            </Link>
+                          </StyledTdMini>
+                        </li>
+                      )
+                  )}
+                </ul>
+              </StyledLi>
+            </Col>
+          )}
+          {!showRisk?.every((a) => a === false) && homepageSearch !== "" && (
+            <Col md={3}>
+              <StyledLi>
+                <Names> Risks</Names>
+                <ul>
+                  {risks?.map(
+                    (risk) =>
+                      risk?.name
+                        ?.toLowerCase()
+                        .includes(homepageSearch.toLowerCase()) && (
+                        <li>
+                          <StyledTdMini key={risk.id}>
+                            <Link to={`/policy/${id}/details/#risks`}>
+                              {risk.name}
+                            </Link>
+                          </StyledTdMini>
+                        </li>
+                      )
+                  )}
+                </ul>
+              </StyledLi>
+            </Col>
+          )}
+          {!showControl?.every((a) => a === false) && homepageSearch !== "" && (
+            <Col md={3}>
+              <StyledLi>
+                <Names>Controls:</Names>
+                <ul>
+                  {controls?.map(
+                    (control) =>
+                      control?.description
+                        ?.toLowerCase()
+                        .includes(homepageSearch.toLowerCase()) && (
+                        <li>
+                          <StyledTdMini key={control.id}>
+                            <Link to={`/policy/${id}/details/#controls`}>
+                              {control.description}
+                            </Link>
+                          </StyledTdMini>
+                        </li>
+                      )
+                  )}
+                </ul>
+              </StyledLi>
+            </Col>
+          )}
+        </Row>
+      </PolicySearchItemContainerMini>
+      {/* <PolicySearchItemContainer>
+          <AttributeSection>
+            {noAttribute ? (
+              <EmptyAttribute>No Attribute</EmptyAttribute>
+            ) : (
+              <StyledUl>
+                {risks?.length ? (
+                  <StyledLi>
+                    <Names> Risks</Names>
+                    <ul>
+                      {risks?.map((risk) => (
+                        <StyledTd key={risk.id}>
+                          <Link to={`/policy/${id}/details/#risks`}>
+                            {risk.name}
+                          </Link>
+                        </StyledTd>
+                      ))}
+                    </ul>
+                  </StyledLi>
+                ) : null}
+                {controls?.length ? (
+                  <StyledLi>
+                    <Names>Controls:</Names>
+                    <ul>
+                      {controls?.map((control) => (
+                        <StyledTd key={control.id}>
+                          <Link to={`/policy/${id}/details/#controls`}>
+                            {control.description}
+                          </Link>
+                        </StyledTd>
+                      ))}
+                    </ul>
+                  </StyledLi>
+                ) : null}
+                {resources?.length ? (
+                  <StyledLi>
+                    <Names>Resources:</Names>
+                    <ul>
+                      {resources?.map((resource) => (
+                        <StyledTd key={resource.id}>
+                          <Link to={`/policy/${id}/resources`}>
+                            {resource.name}
+                          </Link>
+                        </StyledTd>
+                      ))}
+                    </ul>
+                  </StyledLi>
+                ) : null}
+                {references?.length ? (
+                  <StyledLi>
+                    <Names>References:</Names>
+                    <ul>
+                      {references?.map((reference) => (
+                        <StyledTd key={reference.id}>
+                          <Link to={`/policy/${id}/details/#references`}>
+                            {reference.name}
+                          </Link>
+                        </StyledTd>
+                      ))}
+                    </ul>
+                  </StyledLi>
+                ) : null}
+              </StyledUl>
+            )}
+          </AttributeSection>
+          <TitleAndDescriptionSection>
+            <StyledLink to={`/policy/${id}/details`}>
+              <div className="text-right">
+                <DateHover withIcon>{updatedAt}</DateHover>
+              </div>
+              <div>
+                <StyledTitle>
+                  <strong>{title}</strong>
+                </StyledTitle>
+              </div>
+              <div className="text-secondary">
+                {previewHtml(description || "")}
+              </div>
+            </StyledLink>
+          </TitleAndDescriptionSection>
+        </PolicySearchItemContainer>
+      )} */}
     </Fragment>
   );
 }
@@ -220,25 +251,25 @@ const showRisk= risks?.map(a=>a.name?.toLowerCase().includes(homepageSearch.toLo
 // =========================================================
 // Three Important Part
 // =========================================================
-const PolicySearchItemContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  background: #f7f7f7;
-  align-items: center;
-  justify-content: space-between;
-  padding: 10px 15px;
-  margin-bottom: 10px;
-  margin-top: 20px;
-  &:last-child {
-    margin-bottom: 0px;
-  }
-  border-radius: 5px;
-  overflow: hidden;
-  /* for small screen */
-  @media screen and (max-width: 767px) {
-    flex-direction: column;
-  }
-`;
+// const PolicySearchItemContainer = styled.div`
+//   display: flex;
+//   flex-direction: row;
+//   background: #f7f7f7;
+//   align-items: center;
+//   justify-content: space-between;
+//   padding: 10px 15px;
+//   margin-bottom: 10px;
+//   margin-top: 20px;
+//   &:last-child {
+//     margin-bottom: 0px;
+//   }
+//   border-radius: 5px;
+//   overflow: hidden;
+//   /* for small screen */
+//   @media screen and (max-width: 767px) {
+//     flex-direction: column;
+//   }
+// `;
 const PolicySearchItemContainerMini = styled.div`
   /* display: flex;
   flex-direction: row; */
@@ -258,26 +289,26 @@ const PolicySearchItemContainerMini = styled.div`
     flex-direction: column;
   }
 `;
-const TitleAndDescriptionSection = styled.div`
-  width: 60%;
-  margin-left: 1.5rem;
-  /* for small screen */
-  @media screen and (max-width: 767px) {
-    order: 1;
-    width: 100%;
-    margin: unset;
-  }
-`;
-const AttributeSection = styled.div`
-  width: 40%;
-  border-right: 1px solid #d85604;
-  /* for small screen */
-  @media screen and (max-width: 767px) {
-    order: 2;
-    width: 100%;
-    border-right: unset;
-  }
-`;
+// const TitleAndDescriptionSection = styled.div`
+//   width: 60%;
+//   margin-left: 1.5rem;
+//   /* for small screen */
+//   @media screen and (max-width: 767px) {
+//     order: 1;
+//     width: 100%;
+//     margin: unset;
+//   }
+// `;
+// const AttributeSection = styled.div`
+//   width: 40%;
+//   border-right: 1px solid #d85604;
+//   /* for small screen */
+//   @media screen and (max-width: 767px) {
+//     order: 2;
+//     width: 100%;
+//     border-right: unset;
+//   }
+// `;
 
 // =========================================================
 // Aditional Component
@@ -296,9 +327,9 @@ const StyledLi = styled.li`
   left: -40px;
 `;
 
-const StyledUl = styled.ul`
-  list-style-type: none;
-`;
+// const StyledUl = styled.ul`
+//   list-style-type: none;
+// `;
 
 const StyledTitle = styled.h4`
   & :hover {
@@ -306,15 +337,14 @@ const StyledTitle = styled.h4`
   }
 `;
 
-const StyledTd = styled.li`
-  &:hover {
-    color: #d85604;
-  }
-  font-size: 13px;
-  word-break: break-all;
-  width: 140%;
-  
-`;
+// const StyledTd = styled.li`
+//   &:hover {
+//     color: #d85604;
+//   }
+//   font-size: 13px;
+//   word-break: break-all;
+//   width: 140%;
+// `;
 
 const StyledTdMini = styled.li`
   &:hover {
