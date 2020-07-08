9880f8408d836f67327dd3a75d2b4c5488096890
As'ad Saleh Umar <asad@rubyh.co>
2020-07-08 10:59:48 +0700

    Fixing Footer

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -1,17 +1,39 @@
 import React, { Fragment } from "react";
 const Footer = () => {
   return (
-    <Fragment>
-      <hr />
-      <div className="justify-content-center d-flex">
-        <p style={{ position: "relative", color: "rgba(0,0,0,.5)" }}>
+    <div
+      style={{
+        display: "block",
+        padding: "20px",
+        height: "60px",
+        width: "100%",
+      }}
+    >
+      <div
+        className="justify-content-center d-flex  "
+        style={{
+          // backgroundColor: "#F8F8F8",
+          borderTop: "1px solid #E7E7E7",
+          padding: "5px",
+          position: "fixed",
+          bottom: "-5px",
+          height: "40px",
+          // width: "100%",
+        }}
+      >
+        <p
+          style={{
+            // position: "fixed",
+            color: "rgba(0,0,0,.5)",
+          }}
+        >
           &copy; 2020 PwC. PwC all rights reserved. PwC refers to the PwC
           network and/or one or more of its member firms, each of which is a
           separate legal entity. Please see www.pwc.com/structure for further
           details.
         </p>
       </div>
-    </Fragment>
+    </div>
   );
 };
 export default Footer;
