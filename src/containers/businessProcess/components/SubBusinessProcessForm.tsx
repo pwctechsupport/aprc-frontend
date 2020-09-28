576b1096230a5cd58a54ba9fd5d8a108bf765fc8
Koza <koza@rubyh.co>
2020-09-28 19:53:51 +0700

    sub bispro form

--- src/containers/businessProcess/components/SubBusinessProcessForm.tsx
+++ src/containers/businessProcess/components/SubBusinessProcessForm.tsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
-import { Form, Input } from "reactstrap";
+import { Form, FormFeedback, FormText, Input } from "reactstrap";
 import { useCreateSubBusinessProcessMutation } from "../../../generated/graphql";
 import DialogButton from "../../../shared/components/DialogButton";
 
 const SubBusinessProcessForm = ({ parentId }: SubBusinessProcessFormProps) => {
   const { register, handleSubmit, reset } = useForm();
+  const [error, setError] = useState(false);
   const [createSubBusinessProcess] = useCreateSubBusinessProcessMutation({
     onCompleted: () => {
       toast.success("Create Success");
@@ -23,35 +24,48 @@ const SubBusinessProcessForm = ({ parentId }: SubBusinessProcessFormProps) => {
   });
 
   function submit(values: any) {
-    createSubBusinessProcess({
-      variables: {
-        input: {
-          name: values.name,
-          parentId,
+    if (values.name) {
+      setError(false);
+      createSubBusinessProcess({
+        variables: {
+          input: {
+            name: values.name,
+            parentId,
+          },
         },
-      },
-    });
+      });
+    } else {
+      setError(true);
+    }
   }
   return (
-    <Form
-      onSubmit={handleSubmit(submit)}
-      className="d-flex align-items-center mb-4"
-    >
-      <Input
-        name="name"
-        placeholder="Sub business process name*"
-        innerRef={register}
-        required
-      />
-      <DialogButton
-        type="button"
-        onConfirm={handleSubmit(submit)}
-        className="button pwc ml-3"
-        message="Add sub business process?"
+    <>
+      <Form
+        onSubmit={handleSubmit(submit)}
+        className="d-flex align-items-center mb-4"
       >
-        Add
-      </DialogButton>
-    </Form>
+        <Input
+          name="name"
+          placeholder="Sub business process name*"
+          innerRef={register}
+          required
+          invalid={error}
+        />
+
+        <DialogButton
+          type="button"
+          onConfirm={handleSubmit(submit)}
+          className="button pwc ml-3"
+          message="Add sub business process?"
+        >
+          Add
+        </DialogButton>
+      </Form>
+      <FormFeedback>{error}</FormFeedback>
+      <FormText style={{ position: "relative", top: -15 }}>
+        {error && "Sub Business Process is a required field"}
+      </FormText>
+    </>
   );
 };
 
