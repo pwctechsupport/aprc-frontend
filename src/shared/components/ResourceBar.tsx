f31c67fd53bb0c9276b381a35e844ad00c98dbf7
Hanggi Anggono <anggonohanggi@gmail.com>
2020-10-10 15:01:59 +0700

    ResourceBar responsive

--- src/shared/components/ResourceBar.tsx
+++ src/shared/components/ResourceBar.tsx
@@ -1,27 +1,27 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import { Button } from "reactstrap";
-import styled from "styled-components";
-import PickIcon from "../../assets/Icons/PickIcon";
-import { useUpdateResourceVisitMutation } from "../../generated/graphql";
-import { APP_ROOT_URL } from "../../settings";
-import useAccessRights from "../hooks/useAccessRights";
-import DialogButton from "./DialogButton";
-import StarRating from "./StarRating";
-import Tooltip from "./Tooltip";
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { Button, Row, Col } from 'reactstrap'
+import styled from 'styled-components'
+import PickIcon from '../../assets/Icons/PickIcon'
+import { useUpdateResourceVisitMutation } from '../../generated/graphql'
+import { APP_ROOT_URL } from '../../settings'
+import useAccessRights from '../hooks/useAccessRights'
+import DialogButton from './DialogButton'
+import StarRating from './StarRating'
+import Tooltip from './Tooltip'
 
 interface ResourceBarProps {
-  id: string;
-  name?: string | null;
-  resuploadUrl?: string | null;
-  rating?: number | null;
-  visit?: number | null;
-  totalRating?: number | null;
-  deleteResource?: any;
-  resourceId?: any;
-  policyIdsWithoutChildren?: any;
-  setResourceId?: any;
-  bPId?: any;
+  id: string
+  name?: string | null
+  resuploadUrl?: string | null
+  rating?: number | null
+  visit?: number | null
+  totalRating?: number | null
+  deleteResource?: any
+  resourceId?: any
+  policyIdsWithoutChildren?: any
+  setResourceId?: any
+  bPId?: any
 }
 
 export default function ResourceBar({
@@ -37,96 +37,98 @@ export default function ResourceBar({
   resourceId,
   totalRating = 0,
 }: ResourceBarProps) {
-  const [isAdminReviewer] = useAccessRights(["admin_reviewer"]);
+  const [isAdminReviewer] = useAccessRights(['admin_reviewer'])
   const [updateResourceVisit] = useUpdateResourceVisitMutation({
-    refetchQueries: ["resources", "recentResources", "reviewerResourcesStatus"],
-  });
+    refetchQueries: ['resources', 'recentResources', 'reviewerResourcesStatus'],
+  })
 
   return (
     <ResourceBarContainer>
-      <ResourceBarDivider width="40">
-        <NavLink
-          exact
-          to={
-            policyIdsWithoutChildren
-              ? `/policy/${policyIdsWithoutChildren}/resources/${id}`
-              : `/risk-and-control/${bPId}/resources/${id}`
-          }
-        >
-          <ResourceName
-            onClick={() => {
-              setResourceId(id);
-            }}
+      <Col xs={8} md={6}>
+        <ResourceBarDivider width="40">
+          <NavLink
+            exact
+            to={
+              policyIdsWithoutChildren
+                ? `/policy/${policyIdsWithoutChildren}/resources/${id}`
+                : `/risk-and-control/${bPId}/resources/${id}`
+            }
           >
-            {name}
-          </ResourceName>
-        </NavLink>
-      </ResourceBarDivider>
-
-      <ResourceBarDivider width="20" align="right">
-        <Button color="transparent">
-          <Tooltip
-            description="Open File"
-            subtitle="Will be download if file type not supported"
-          >
-            <a
-              href={`${APP_ROOT_URL}${resuploadUrl}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              download={`Pwc-Resource ${name}`}
-              onClick={() => updateResourceVisit({ variables: { id } })}
+            <ResourceName
+              onClick={() => {
+                setResourceId(id)
+              }}
             >
-              <PickIcon name="downloadOrange" />
-            </a>
-          </Tooltip>
-        </Button>
-        {isAdminReviewer && (
-          <DialogButton color="" onConfirm={() => deleteResource(resourceId)}>
+              {name}
+            </ResourceName>
+          </NavLink>
+        </ResourceBarDivider>
+      </Col>
+
+      <Col xs={4} md={3}>
+        <ResourceBarDivider width="20" align="right">
+          <Button color="transparent">
             <Tooltip
-              description="Delete Resource"
-              subtitle="Resource will be deleted from current policy"
+              description="Open File"
+              subtitle="Will be download if file type not supported"
             >
-              <PickIcon name="trash" className="clickable" />
+              <a
+                href={`${APP_ROOT_URL}${resuploadUrl}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                download={`Pwc-Resource ${name}`}
+                onClick={() => updateResourceVisit({ variables: { id } })}
+              >
+                <PickIcon name="downloadOrange" />
+              </a>
             </Tooltip>
-          </DialogButton>
-        )}
-      </ResourceBarDivider>
+          </Button>
+          {isAdminReviewer && (
+            <DialogButton color="" onConfirm={() => deleteResource(resourceId)}>
+              <Tooltip
+                description="Delete Resource"
+                subtitle="Resource will be deleted from current policy"
+              >
+                <PickIcon name="trash" className="clickable" />
+              </Tooltip>
+            </DialogButton>
+          )}
+        </ResourceBarDivider>
+      </Col>
 
-      <ResourceBarDivider width="10">
-        <StarRating
-          id={id}
-          rating={rating}
-          totalRating={totalRating}
-          // onStarClick={handleStarClick}
-        />
-        <ResourceViewCount className="views">{visit} Views</ResourceViewCount>
-      </ResourceBarDivider>
+      <Col xs={12} md={3}>
+        <ResourceBarDivider width="10">
+          <StarRating
+            id={id}
+            rating={rating}
+            totalRating={totalRating}
+            // onStarClick={handleStarClick}
+          />
+          <ResourceViewCount className="views">{visit} Views</ResourceViewCount>
+        </ResourceBarDivider>
+      </Col>
     </ResourceBarContainer>
-  );
+  )
 }
 
 // ------------------------------------------------
 // Styled Components Constructor
 // ------------------------------------------------
 
-const ResourceBarContainer = styled.div`
+const ResourceBarContainer = styled(Row)`
   background: #fbeee6;
-  display: flex;
-  flex-direction: row;
   align-items: center;
-  justify-content: space-between;
   padding: 10px 15px;
   margin-bottom: 10px;
   & :last-child {
     margin-bottom: 0px;
   }
-`;
+`
 
 const ResourceBarDivider = styled.div<{ width?: string; align?: string }>`
-  width: ${(p) => p.width + "%"};
   text-align: ${(p) => p.align};
   margin: 0px 5px;
-`;
+`
 
 const ResourceName = styled.div`
   font-weight: bold;
@@ -134,8 +136,8 @@ const ResourceName = styled.div`
   line-height: 20px;
   color: #d85604;
   cursor: pointer;
-`;
+`
 
 const ResourceViewCount = styled.div`
   font-size: 12px;
-`;
+`
