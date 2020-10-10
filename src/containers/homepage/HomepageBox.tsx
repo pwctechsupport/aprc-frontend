1d75f0326e5547abbf936f0265c1e49ad452c766
Hanggi Anggono <anggonohanggi@gmail.com>
2020-10-10 10:36:10 +0700

    prettier HomepageBox

--- src/containers/homepage/HomepageBox.tsx
+++ src/containers/homepage/HomepageBox.tsx
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
-import { FaCaretRight } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import { Collapse } from "reactstrap";
-import styled, { css } from "styled-components";
-import EmptyAttribute from "../../shared/components/EmptyAttribute";
-import { Suggestions } from "../../shared/formatter";
-import useWindowSize from "../../shared/hooks/useWindowSize";
+import React, { useState } from 'react'
+import { FaCaretRight } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import { Collapse } from 'reactstrap'
+import styled, { css } from 'styled-components'
+import EmptyAttribute from '../../shared/components/EmptyAttribute'
+import { Suggestions } from '../../shared/formatter'
+import useWindowSize from '../../shared/hooks/useWindowSize'
 
 interface HomepageBoxProps {
-  list: Suggestions;
-  title?: string | null;
-  basePath: string;
-  themeColor?: string;
-  fontColor?: string;
+  list: Suggestions
+  title?: string | null
+  basePath: string
+  themeColor?: string
+  fontColor?: string
 }
 
 export default function HomepageBox({
@@ -20,26 +20,28 @@ export default function HomepageBox({
   title,
   basePath,
   themeColor,
-  fontColor = "white",
+  fontColor = 'white',
 }: HomepageBoxProps) {
-  const [open, setOpen] = useState(true);
-  const [textHeight, setTextHeight] = useState(0);
-  const { height } = useWindowSize();
-  const heigthAdjustment = open ? `${(height * 50) / 100 - textHeight}px` : undefined;
+  const [open, setOpen] = useState(true)
+  const [textHeight, setTextHeight] = useState(0)
+  const { height } = useWindowSize()
+  const heigthAdjustment = open
+    ? `${(height * 50) / 100 - textHeight}px`
+    : undefined
   return (
     <div
       className="my-2"
       style={{
         backgroundColor: themeColor,
-        boxShadow: "3px 6px 20px 0px rgba(0,0,0,0.3)",
-        borderRadius: "3px",
-        border: "1px solid rgba(0,0,0,0.1)",
+        boxShadow: '3px 6px 20px 0px rgba(0,0,0,0.3)',
+        borderRadius: '3px',
+        border: '1px solid rgba(0,0,0,0.1)',
       }}
     >
       <div
         className="d-flex justify-content-between align-items-flex-start p-2 pt-3"
         style={{
-          borderBottom: "1px solid rgba(0,0,0,0.3)",
+          borderBottom: '1px solid rgba(0,0,0,0.3)',
         }}
         ref={(ref) => setTextHeight(ref?.clientHeight || 0)}
       >
@@ -49,36 +51,36 @@ export default function HomepageBox({
         </BoxHeader>
       </div>
       {/* <Collapse isOpen={open}> */}
-        <div className="p-2" style={{ minHeight: heigthAdjustment }}>
-          {list.length ? (
-            list.slice(0, open ? 7 : 3).map((item) => (
-              <StyledLink
-                key={item.value}
-                to={
-                  basePath === "policy"
-                    ? `/${basePath}/${item.value}/details`
-                    : `/${basePath}/${item.value}`
-                }
-                style={{ color: fontColor }}
-                className="d-flex align-items-center my-2"
-              >
-                <div className="mr-3">
-                  <Circle>{item.label.charAt(0).toUpperCase()}</Circle>
-                </div>
-                <StyledSpan>{item.label}</StyledSpan>
-              </StyledLink>
-            ))
-          ) : (
-            <div style={{ paddingTop: "13vh" }}>
-              <EmptyAttribute style={{ color: fontColor }}>
-                No {title}
-              </EmptyAttribute>
-            </div>
-          )}
-        </div>
+      <div className="p-2" style={{ minHeight: heigthAdjustment }}>
+        {list.length ? (
+          list.slice(0, open ? 7 : 3).map((item) => (
+            <StyledLink
+              key={item.value}
+              to={
+                basePath === 'policy'
+                  ? `/${basePath}/${item.value}/details`
+                  : `/${basePath}/${item.value}`
+              }
+              style={{ color: fontColor }}
+              className="d-flex align-items-center my-2"
+            >
+              <div className="mr-3">
+                <Circle>{item.label.charAt(0).toUpperCase()}</Circle>
+              </div>
+              <StyledSpan>{item.label}</StyledSpan>
+            </StyledLink>
+          ))
+        ) : (
+          <div style={{ paddingTop: '13vh' }}>
+            <EmptyAttribute style={{ color: fontColor }}>
+              No {title}
+            </EmptyAttribute>
+          </div>
+        )}
+      </div>
       {/* </Collapse> */}
     </div>
-  );
+  )
 }
 const StyledSpan = styled.span`
   overflow: hidden;
@@ -87,7 +89,7 @@ const StyledSpan = styled.span`
   -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
   max-height: 3.6em;
-`;
+`
 
 const StyledLink = styled(Link)`
   color: rgba(0, 0, 0, 0.5);
@@ -98,7 +100,7 @@ const StyledLink = styled(Link)`
     color: rgba(0, 0, 0, 0.8);
   }
   padding: 5px;
-`;
+`
 
 const Circle = styled.div`
   width: 30px;
@@ -112,7 +114,7 @@ const Circle = styled.div`
   ${StyledLink}:hover & {
     background: rgba(0, 0, 0, 0.8);
   }
-`;
+`
 
 const BoxHeader = styled.div`
   padding: 5px 7px;
@@ -122,7 +124,7 @@ const BoxHeader = styled.div`
   &:hover {
     background: rgba(0, 0, 0, 0.1);
   }
-`;
+`
 
 const Icon = styled(FaCaretRight)<{ open: boolean }>`
   transition: 0.15s ease-in-out;
@@ -131,4 +133,4 @@ const Icon = styled(FaCaretRight)<{ open: boolean }>`
     css`
       transform: rotate(90deg);
     `};
-`;
+`
