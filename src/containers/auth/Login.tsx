685f60856d36b0da6a919acbeaa67737faf8a403
Benjamin Sitompul <benjamin@rubyh.co>
2020-11-06 17:56:03 +0700

    change position of copyright for all pages

--- src/containers/auth/Login.tsx
+++ src/containers/auth/Login.tsx
@@ -139,7 +139,7 @@ export default function Login({ history }: RouteComponentProps) {
           </LoginBox>
         </Col>
       </Row>
-      <Footer fontColor={'white'} linebreak />
+      <Footer fontColor={'white'} linebreak origin={'login'} />
     </BsContainer>
   )
 }
