0df7daa8ca7979b04a9ef194ba6f904f97d64ebf
Frieda <frieda@altechomega.com>
2022-07-14 16:48:37 +0700

    refactor(): Change label Validation to Validity

--- src/containers/control/components/ControlForm.tsx
+++ src/containers/control/components/ControlForm.tsx
@@ -649,7 +649,7 @@ const newLabelTypeOfControls = [
 const newLabelIPOs = [
   {label: "Completeness", value: "completeness"},
   {label: "Accuracy", value: "accuracy"},
-  {label: "Validation", value: "validation"},
+  {label: "Validity", value: "validity"},
   {label: "Restriction Access (CAVR)", value: "restriction"}
 ]
 
